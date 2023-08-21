f157833d43be27611bd55938866a4aef8365347d
Anshu Sharma <anshusharma6327@gmail.com>
2023-08-21 17:00:39 +0530

    headers added for nginx in readme

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require("http");
 const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/json");
   const ip =
-    req.headers["cf-connecting-ip"] ||
-    req.headers["x-real-ip"] ||
-    req.headers["x-forwarded-for"] ||
-    req.socket.remoteAddress ||
-    "";
+  req.headers["cf-connecting-ip"] ||
+  req.headers["x-real-ip"] ||
+  req.headers["x-forwarded-for"] ||
+  req.socket.remoteAddress ||
+  "";
+  res.setHeader("Content-Type", "text/json");
   res.end(JSON.stringify({ ip }));
 });
 
