8d1316ebe08cc8dd8226745a507c9f9a51d4bc09
Anshu Sharma <anshusharma6327@gmail.com>
2024-01-21 15:09:52 +0530

    return html

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const ip =
-  req.headers["cf-connecting-ip"] ||
-  req.headers["x-real-ip"] ||
-  req.headers["x-forwarded-for"] ||
-  req.socket.remoteAddress ||
-  "";
-  res.setHeader("Content-Type", "text/json");
-  res.end(JSON.stringify({ ip }));
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress ||
+    "";
+  res.setHeader("Content-Type", "text/html");
+  res.end(`<p>${ip}</p>`);
 });
 
 server.listen(PORT, () => {
